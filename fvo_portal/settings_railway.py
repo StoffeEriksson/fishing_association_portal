87f5c45f6a9f1d361fa66f21dfc589c1f6585ec5
StoffeEriksson <djmr0kuk@students.codeinstitute.net>
2026-04-14 21:41:30 +0200

    Disable organization middleware on Railway

--- fvo_portal/settings_railway.py
+++ fvo_portal/settings_railway.py
@@ -3,22 +3,10 @@ import os
 import dj_database_url
 
 DEBUG = False
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
-railway_public_domain = os.getenv("RAILWAY_PUBLIC_DOMAIN")
-if railway_public_domain:
-    ALLOWED_HOSTS.append(railway_public_domain)
-
-CSRF_TRUSTED_ORIGINS = []
-if railway_public_domain:
-    CSRF_TRUSTED_ORIGINS.append(f"https://{railway_public_domain}")
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        ssl_require=False,
-    )
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=False)
 }
 
 MIDDLEWARE = [
